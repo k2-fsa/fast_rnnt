371e2657b73158c594c7dd1fdd555b09ea071c89
Daniel Povey <dpovey@gmail.com>
2021-07-26 13:03:23 +0800

    Start changing some names

--- setup.py
+++ setup.py
@@ -9,48 +9,31 @@ with open('requirements.txt') as f:
 
 
 long_description = """
-This package implements an efficient parallel algorithm for the computation of discounted cumulative sums
-with differentiable bindings to PyTorch. The `cumsum` operation is frequently seen in data science
-domains concerned with time series, including Reinforcement Learning (RL).
+This package implements an efficient parallel algorithm for the computation of
+mutual information between sequences with differentiable bindings to PyTorch.
 
-The traditional sequential algorithm performs the computation of the output elements in a loop. For an input of size
-`N`, it requires `O(N)` operations and takes `O(N)` time steps to complete.
-
-The proposed parallel algorithm requires a total of `O(N log N)` operations, but takes only `O(log N)` time, which is a
-considerable trade-off in many applications involving large inputs.
-
-Features of the parallel algorithm:
-- Speed logarithmic in the input size
-- Better numerical precision than sequential algorithms
-
-Features of the package:
-- CPU: sequential algorithm in C++
-- GPU: parallel algorithm in CUDA
-- Gradients computation wrt input
-- Both left and right directions of summation supported
-- PyTorch bindings
 
 Find more details and the most up-to-date information on the project webpage:
-https://www.github.com/toshas/torch-discounted-cumsum
+[TODO]
 """
 
 
 def configure_extensions():
     out = [
         CppExtension(
-            'torch_learned_nonlin_cpu',
+            'torch_mutual_information_cpu',
             [
-                os.path.join('torch_learned_nonlin', 'learned_nonlin_cpu.cpp'),
+                os.path.join('torch_mutual_information', 'mutual_information_cpu.cpp'),
             ],
         )
     ]
     try:
         out.append(
             CUDAExtension(
-                'torch_learned_nonlin_cuda',
+                'torch_mutual_information_cuda',
                 [
-                    os.path.join('torch_learned_nonlin', 'learned_nonlin_cuda.cpp'),
-                    os.path.join('torch_learned_nonlin', 'learned_nonlin_cuda_kernel.cu'),
+                    os.path.join('torch_mutual_information', 'mutual_information_cuda.cpp'),
+                    os.path.join('torch_mutual_information', 'mutual_information_cuda_kernel.cu'),
                 ],
             )
         )
@@ -60,9 +43,9 @@ def configure_extensions():
 
 
 setup(
-    name='torch_learned_nonlin',
+    name='torch_mutual_information',
     version='1.0.2',
-    description='Fast discounted cumulative sums in PyTorch',
+    description='Mutual information between sequences of vectors',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=requirements,
@@ -70,13 +53,11 @@ setup(
     packages=find_packages(),
     author='Dan Povey',
     license='BSD',
-    url='https://www.github.com/toshas/torch-discounted-cumsum',
     ext_modules=configure_extensions(),
     cmdclass={
         'build_ext': BuildExtension
     },
     keywords=[
-        'pytorch', 'discounted', 'cumsum', 'cumulative', 'sum', 'scan', 'differentiable',
-        'reinforcement', 'learning', 'rewards', 'time', 'series'
+        'pytorch', 'sequence', 'mutual', 'information'
     ],
 )
