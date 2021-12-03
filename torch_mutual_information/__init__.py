5e0df7e8821d19583aec47c5e8768d2398478fd2
Daniel Povey <dpovey@gmail.com>
2021-12-03 12:58:32 +0800

    Add aux version of rnnt loss, allows to have "lm-only" and "am-only" versions of the loss which may add regularization, and also ensure that the ILM probs make sense independently.

--- torch_mutual_information/__init__.py
+++ torch_mutual_information/__init__.py
@@ -1,2 +1,2 @@
 from .mutual_information import mutual_information_recursion, joint_mutual_information_recursion
-from .rnnt import get_rnnt_logprobs, rnnt_loss_simple
+from .rnnt import get_rnnt_logprobs, rnnt_loss_simple, rnnt_loss_aux
