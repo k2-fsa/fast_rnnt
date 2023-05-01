91d53532ec7456d46554f75c94b94ca3edf7195d
Desh Raj <r.desh26@gmail.com>
2023-05-01 19:35:35 -0400

    remove unwanted change

--- fast_rnnt/python/fast_rnnt/__init__.py
+++ fast_rnnt/python/fast_rnnt/__init__.py
@@ -13,6 +13,3 @@ from .rnnt_loss import rnnt_loss
 from .rnnt_loss import rnnt_loss_pruned
 from .rnnt_loss import rnnt_loss_simple
 from .rnnt_loss import rnnt_loss_smoothed
-
-
-__version__ = '1.2'
